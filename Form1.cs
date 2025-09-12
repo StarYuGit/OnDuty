eebd0890faf16074536d045e9f7ebfcd7aab214f
STARYU6B40\staryu <japenjustnew@gmail.com>
2025-09-12 16:51:46 +0800

    1.add model 2. codeing

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using OnDuty.Models;
+
 namespace OnDuty
 {
     public partial class Form1 : Form
@@ -15,31 +17,33 @@ namespace OnDuty
 
         private void btn_InputHoliDay_Click(object sender, EventArgs e)
         {
-            using (OpenFileDialog openFileDialog = new OpenFileDialog())
-            {
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    tb_InputHoliDay.Text = openFileDialog.FileName;
+            //using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            //{
+            //    if (openFileDialog.ShowDialog() == DialogResult.OK)
+            //    {
+                    //tb_InputHoliDay.Text = openFileDialog.FileName;
+                    tb_InputHoliDay.Text = "C:\\Projects\\OnDuty\\114年中華民國政府行政機關辦公日曆表(修正版).csv";
+
                     string holidayFile = tb_InputHoliDay.Text;
                     label1.Text = "";
                     using (StreamReader holiday = new StreamReader(holidayFile))
                     {
-             
+                        List<ScheduleDate> scheduleDates = [];
                         string? line;
                         bool isWorkDayData = false;
                         while ((line = holiday.ReadLine()) != null)
                         {
-                            if ("西元日期,星期,是否放假,備註".Equals(line))
-                                isWorkDayData = true;
                             if (isWorkDayData)
                             {
                                 label1.Text += line + Environment.NewLine;
+                                List<string> everyDay = line.Split(',').ToList();
                             }
-     
+                            if ("西元日期,星期,是否放假,備註".Equals(line))
+                                isWorkDayData = true;
                         }
                     }
-                }
-            }
+            //    }
+            //}
         }
     }
 }
---
+++ Models/ScheduleDate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnDuty.Models
+{
+    public class ScheduleDate
+    {
+        public ScheduleDate() {}
+        public ScheduleDate(int fullDate, string year, string month, string day, string week, int dayType, string remark)
+        {
+            this.fullDate = fullDate;
+            this.year = year;
+            this.month = month;
+            this.day = day;
+            this.week = week;
+            this.dayType = dayType;
+            this.remark = remark;
+        }
+        public int fullDate { get; set; }
+        public string? year { get; set; }
+        public string? month { get; set; }
+        public string? day { get; set; }
+        public string? week { set; get; }
+        public string? dayType { get; set; }
+        public string? remark { set; get; } = null;
+    }
+}
